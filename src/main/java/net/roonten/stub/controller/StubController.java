faf3ba9c2f3f55b4504927e90d14abc2eabe566f
tianlong <tianlong@eidlink.com>
2018-04-19 15:56:06 +0800

    解决性能线程问题。

--- src/main/java/net/roonten/stub/controller/StubController.java
+++ src/main/java/net/roonten/stub/controller/StubController.java
@@ -19,73 +19,75 @@ import net.roonten.stub.utils.IOUtils;
 @Controller
 public class StubController {
 
-	private static Log log = LogFactory.getLog(StubController.class);
+    private static Log log = LogFactory.getLog(StubController.class);
 
-	private static Properties properties = new Properties();
+    private static Properties properties = new Properties();
 
-	private static final Object lock = new Object();
+    private static final Object lock = new Object();
 
-	private static boolean reloading = false;
+    private static boolean reloading = false;
 
-	static {
-		new Thread() {
-			public void run() {
-				while (true) {
-					synchronized (lock) {
-						reloading = true;
-						try {
-							properties.clear();
-							properties.load(StubController.class.getResourceAsStream("/conf/stub-config.properties"));
-						} catch (Throwable ex) {
-						}
-						reloading = false;
-						lock.notifyAll();
-					}
-					try {
-						// 5秒更新一次配置
-						Thread.sleep(5000);
-					} catch (InterruptedException e) {
-					}
-				}
-			}
-		}.start();
-	}
+    static {
+        new Thread() {
+            public void run() {
+                while (true) {
+                    synchronized (lock) {
+                        reloading = true;
+                        try {
+                            properties.clear();
+                            properties.load(StubController.class.getResourceAsStream("/conf/stub-config.properties"));
+                        } catch (Throwable ex) {
+                        }
+                        reloading = false;
+                        lock.notifyAll();
+                    }
+                    try {
+                        // 5秒更新一次配置
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }.start();
+    }
 
-	@RequestMapping("/stub/{i}")
-	public void doStubIndex(@PathVariable("i") String i, HttpServletRequest request, HttpServletResponse response) throws IOException {
-		synchronized (lock) {
-			while (reloading) {
-				try {
-					lock.wait();
-				} catch (InterruptedException ex) {
-				}
-			}
+    @RequestMapping("/stub/{i}")
+    public void doStubIndex(@PathVariable("i") String i, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (reloading) {
+            synchronized (lock) {
+                while (reloading) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        }
 
-			String contentType = properties.getProperty("stub[" + i + "].response.contentType").intern();
-			if (contentType == null || contentType.trim().intern().length() == 0) {
-				response.sendError(404, "stub[" + i + "].response.contentType 未配置，请检查 conf/stub-config.properties");
-				return;
-			}
-			String data = properties.getProperty("stub[" + i + "].response.data");
-			if (data == null || data.trim().length() == 0) {
-				response.sendError(404, "stub[" + i + "].response.data 未配置，请检查 conf/stub-config.properties");
-				return;
-			}
-			log.info("stub/" + i + " 收到一个请求： response.contentType=" + contentType + ", response.data=" + data);
+        String contentType = properties.getProperty("stub[" + i + "].response.contentType").intern();
+        if (contentType == null || contentType.trim().intern().length() == 0) {
+            response.sendError(404, "stub[" + i + "].response.contentType 未配置，请检查 conf/stub-config.properties");
+            return;
+        }
+        String data = properties.getProperty("stub[" + i + "].response.data");
+        if (data == null || data.trim().length() == 0) {
+            response.sendError(404, "stub[" + i + "].response.data 未配置，请检查 conf/stub-config.properties");
+            return;
+        }
+        log.info("stub/" + i + " 收到一个请求： response.contentType=" + contentType + ", response.data=" + data);
 
-			byte[] b;
-			if (data.startsWith("[BINARY:]")) {
-				b = Base64.decodeBase64(data.substring(9).trim());
-			} else {
-				b = data.getBytes(Charsets.UTF_8);
-			}
+        byte[] b;
+        if (data.startsWith("[BINARY:]")) {
+            b = Base64.decodeBase64(data.substring(9).trim());
+        } else {
+            b = data.getBytes(Charsets.UTF_8);
+        }
 
-			response.setContentType(contentType);
-			try {
-				IOUtils.output(b, response.getOutputStream());
-			} catch (Throwable ex) {
-				log.warn(ex.getMessage(), ex);
-			}
-		}
-	}
+        response.setContentType(contentType);
+        try {
+            IOUtils.output(b, response.getOutputStream());
+        } catch (Throwable ex) {
+            log.warn(ex.getMessage(), ex);
+        }
+    }
 }
